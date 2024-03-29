acd71fa0fbc36d7984085dc329ee17af6a0f6b61
filipbli <filipblicharski2005@gmail.com>
2024-03-29 19:50:30 +0100

    Poprawa graficzna componentow admin, event-form, event-details

--- src/app/event-details/event-details.component.ts
+++ src/app/event-details/event-details.component.ts
@@ -8,17 +8,30 @@ import { EventService } from 'src/services/event.service';
   styleUrls: ['./event-details.component.css']
 })
 export class EventDetailsComponent implements OnInit{
-  constructor(private eventsService: EventService) {
-
-  }
+  constructor(private eventsService: EventService) {}
 
   selectedEvent: EventClass | undefined;
+  imagesList: string[] = [];
 
   ngOnInit() {
+    this.imagesList.push('./assets/plecak.jpg');
+    this.imagesList.push('./assets/polski.jpg');
     this.getEvent();
+    console.log(this.getSelectedImage(this.selectedEvent?.id))
   }
 
   getEvent() {
     this.eventsService.getSelectedEvent().subscribe(event => this.selectedEvent = event);
+  }
+
+  getSelectedImage(id: string | undefined) {
+    if(id != null && this.imagesList.length > 0) {
+      return this.imagesList[parseInt(id)];
+    } 
+    return '';
+  }
+
+  getImage(number: number) {
+    return this.imagesList[number];
   }
 }
